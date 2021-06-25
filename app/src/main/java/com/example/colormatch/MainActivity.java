51f2c6ab6fc23575d5740c8c5ca826c62f3c502b
Ram Michaeli <ram153486@gmail.com>
2021-06-26 00:08:46 +0300

    Added animations

--- app/src/main/java/com/example/colormatch/MainActivity.java
+++ app/src/main/java/com/example/colormatch/MainActivity.java
@@ -6,8 +6,12 @@ import android.app.Dialog;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,10 +19,92 @@ public class MainActivity extends AppCompatActivity {
     ImageButton about_us_bn;
     MediaPlayer song;
 
+
+    Button easyBtn, mediumBtn, hardBtn, dualBtn;
+    Animation scaleUp, scaleDown;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        easyBtn = findViewById(R.id.easy_btn);
+        mediumBtn = findViewById(R.id.medium_btn);
+        hardBtn = findViewById(R.id.hard_btn);
+        dualBtn = findViewById(R.id.dual_btn);
+
+        scaleUp = AnimationUtils.loadAnimation(this, R.anim.scale_up);
+        scaleDown = AnimationUtils.loadAnimation(this,R.anim.scale_down);
+
+        easyBtn.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                if(event.getAction()== MotionEvent.ACTION_DOWN) {
+                    easyBtn.startAnimation(scaleUp);
+                }
+                else if (event.getAction()== MotionEvent.ACTION_UP) {
+                    easyBtn.startAnimation(scaleDown);
+                }
+
+
+                return true;
+            }
+        });
+
+                mediumBtn.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                if(event.getAction()== MotionEvent.ACTION_DOWN) {
+                    mediumBtn.startAnimation(scaleUp);
+                }
+                else if (event.getAction()== MotionEvent.ACTION_UP) {
+                    mediumBtn.startAnimation(scaleDown);
+                }
+
+
+                return true;
+            }
+        });
+
+                hardBtn.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                if(event.getAction()== MotionEvent.ACTION_DOWN) {
+                    hardBtn.startAnimation(scaleUp);
+                }
+                else if (event.getAction()== MotionEvent.ACTION_UP) {
+                    hardBtn.startAnimation(scaleDown);
+                }
+
+
+                return true;
+            }
+        });
+
+                dualBtn.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                if(event.getAction()== MotionEvent.ACTION_DOWN) {
+                    dualBtn.startAnimation(scaleUp);
+                }
+                else if (event.getAction()== MotionEvent.ACTION_UP) {
+                    dualBtn.startAnimation(scaleDown);
+                }
+
+
+                return true;
+            }
+        });
+
+
+
+
+
+
 
         song=MediaPlayer.create(MainActivity.this,R.raw.boy_oh_boy);
         about_us_bn = (ImageButton)findViewById(R.id.about_us_button);
