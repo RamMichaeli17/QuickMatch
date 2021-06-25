9127c0ce5fdd9a90b8f73f30c69de7b9642d4cbc
Yulia <yulia2232@gmail.com>
2021-06-26 00:18:51 +0300

    .

--- app/src/main/java/com/example/colormatch/MainActivity.java
+++ app/src/main/java/com/example/colormatch/MainActivity.java
@@ -6,13 +6,14 @@ import android.app.Dialog;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.view.Window;
 import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageButton about_us_bn;
+    ImageButton about_us_bn, music_bn;
     MediaPlayer song;
 
     @Override
@@ -22,22 +23,30 @@ public class MainActivity extends AppCompatActivity {
 
         song=MediaPlayer.create(MainActivity.this,R.raw.boy_oh_boy);
         about_us_bn = (ImageButton)findViewById(R.id.about_us_button);
+        music_bn=(ImageButton)findViewById(R.id.music_button);
+
         about_us_bn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this,AboutUsPopup.class));
             }
         });
+
+        music_bn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                song.setLooping(true);
+                song.start();
+              /* if (music_bn.callOnClick()){
+                    song.release();
+                }*/
+            }
+        });
     }
- /*   public void Music(View view){
-        song.setLooping(true);
-        song.start();
-    }*/
 
     @Override
     protected void onPause() {
         super.onPause();
-        /*song.release();*/
         song.start();
     }
 
