30509fc46db5913bfde452ceb9c4d5c5a21fe280
Ram Michaeli <ram153486@gmail.com>
2021-06-25 17:10:21 +0300

    Added a splash screen with the logo of the app

---
+++ app/src/main/java/com/example/colormatch/SplashActivity.java
@@ -0,0 +1,41 @@
+package com.example.colormatch;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN_TIMEOUT=2000;  //Splash duration
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.splash_activity);
+
+        Animation fadeOut = new AlphaAnimation(1,0);
+        fadeOut.setInterpolator(new AccelerateInterpolator());
+        fadeOut.setStartOffset(500);
+        fadeOut.setDuration(1800);
+        ImageView imageView= findViewById(R.id.imageView);
+
+        imageView.setAnimation(fadeOut);
+
+        new Handler().postDelayed((Runnable) () -> {
+            Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+            startActivity(intent);
+            finish();
+        },SPLASH_SCREEN_TIMEOUT);
+    }
+}
